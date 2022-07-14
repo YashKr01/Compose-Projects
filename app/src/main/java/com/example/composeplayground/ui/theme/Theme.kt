5a6f3ac8a510445ed661b503781634f216d5c4e9
yashkumar <yash.k@zivame.com>
2022-07-14 12:40:52 +0530

    dependencies & Resources

--- app/src/main/java/com/example/composeplayground/ui/theme/Theme.kt
+++ app/src/main/java/com/example/composeplayground/ui/theme/Theme.kt
@@ -5,26 +5,20 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = ColorPrimary,
+    background = DarkGray,
+    onBackground = TextWhite,
+    onPrimary = DarkGray
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
+    primary = ColorPrimary,
     background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+    onBackground = MediumGray,
+    onPrimary = DarkGray
 )
 
 @Composable
