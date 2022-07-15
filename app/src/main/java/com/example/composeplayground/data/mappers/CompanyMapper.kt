b405fd69566a4d50c1ca553566902a0c739dbbc1
yashkumar <yash.k@zivame.com>
2022-07-15 11:42:52 +0530

    Database setup & Mappers

---
+++ app/src/main/java/com/example/composeplayground/data/mappers/CompanyMapper.kt
@@ -0,0 +1,20 @@
+package com.example.composeplayground.data.mappers
+
+import com.example.composeplayground.data.local.CompanyListingEntity
+import com.example.composeplayground.domain.model.CompanyListing
+
+fun CompanyListingEntity.toCompanyListing(): CompanyListing {
+    return CompanyListing(
+        name = name,
+        symbol = symbol,
+        exchange = exchange
+    )
+}
+
+fun CompanyListing.toCompanyListingEntity(): CompanyListingEntity {
+    return CompanyListingEntity(
+        name = name,
+        symbol = symbol,
+        exchange = exchange
+    )
+}
