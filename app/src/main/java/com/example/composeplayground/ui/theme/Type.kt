f8f3e5c427826d964af94780e7e37175132453a6
yashkumar <yash.k@zivame.com>
2022-09-11 13:40:48 +0530

    Grocery UI

--- app/src/main/java/com/example/composeplayground/ui/theme/Type.kt
+++ app/src/main/java/com/example/composeplayground/ui/theme/Type.kt
@@ -1,28 +1,20 @@
 package com.example.composeplayground.ui.theme
 
 import androidx.compose.material.Typography
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
+import com.example.composeplayground.R
 
-// Set of Material typography styles to start with
+val PoppinsFamily = FontFamily(
+    Font(R.font.poppins_regular),
+    Font(R.font.poppins_semibold, FontWeight.SemiBold),
+)
+
+val NewYorkFamily = FontFamily(
+    Font(R.font.newyork)
+)
+
 val Typography = Typography(
-    body1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
-    ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
-    )
-    */
+    defaultFontFamily = PoppinsFamily
 )
